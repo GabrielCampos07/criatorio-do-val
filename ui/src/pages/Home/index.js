492d01ae3527a3a013b3a2d638485f85c28c6e35
GabrielCampos07 <siginmegain@hotmail.com>
2022-04-17 00:41:55 -0300

    terminando estruturação + criação de header

---
+++ ui/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+
+
+function Home(props) {
+    return (
+        <main>
+            <Header></Header>
+        </main>
+    )
+}
+
+export default Home;
